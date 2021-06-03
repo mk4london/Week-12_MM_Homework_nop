82ce6f0190eeb13840a598188f5aaabbf69ad838
mk4london <81772843+mk4london@users.noreply.github.com>
2021-06-03 14:20:45 +0000

    my week 12 NOP Commerce homework

--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -1,16 +1,12 @@
 package utility;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class Utility {
     public WebDriver driver;
